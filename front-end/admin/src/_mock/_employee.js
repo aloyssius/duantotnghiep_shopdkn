27edd2e86ef878328cfaecb6d896d8b1674af2e2
duongdhph21621 <duongdhph21621@fpt.edu.vn>
2024-08-31 16:59:08 +0700

    them logo image cho giao dien phia admin

--- front-end/admin/src/_mock/_employee.js
+++
@@ -1,7 +0,0 @@
-import _mock from "./_mock";
-
-export const _employee = [...Array(10)].map((_, index) => ({
-  id: _mock.id(index),
-  fullName: `ABCDEFG ${index}`,
-  phoneNumber: `09090909${index}`
-}));
