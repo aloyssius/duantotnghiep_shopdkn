8d811cd0bc37a243abf5c8e594fc7f1c8748e66d
duongdhph21621 <duongdhph21621@fpt.edu.vn>
2024-09-05 22:47:33 +0700

    chinh sua project

--- back-end/app/Common/BaseBlueprint.php
+++ back-end/app/Common/BaseBlueprint.php
@@ -10,38 +10,15 @@ class BaseBlueprint extends Blueprint
     public function baseColumn()
     {
         $this->uuid('id')->primary();
-        $this->timestamps();
+        $this->timestamps(); // created_at, updated_at
         $this->uuid('created_by')->nullable();
         $this->uuid('updated_by')->nullable();
 
         return $this;
     }
 
-    public function addColumnName()
-    {
-        $this->string('name', ConstantSystem::DEFAULT_MAX_LENGTH);
-        return $this;
-    }
-
-    public function addColumnCode()
-    {
-        $this->string('code', ConstantSystem::CODE_MAX_LENGTH)->unique();
-        return $this;
-    }
-
-    public function addSoftDeletes()
-    {
-        $this->softDeletes()->nullable();
-        return $this;
-    }
-
     public function bigDecimal(string $colunmName)
     {
         $this->decimal($colunmName, 15, 2)->default(0);
-    }
-
-    public function bigDecimalNullable(string $colunmName)
-    {
-        $this->decimal($colunmName, 15, 2)->nullable();
     }
 }
--- back-end/app/Helpers/ApiResponse.php
+++ back-end/app/Helpers/ApiResponse.php
@@ -10,17 +10,6 @@ use function PHPUnit\Framework\isEmpty;
 
 class ApiResponse
 {
-    public static function rollback($e, $message = "Something went wrong! Process not completed")
-    {
-        DB::rollBack();
-        self::throw($e, $message);
-    }
-
-    public static function throw($e, $message = "Something went wrong! Process not completed")
-    {
-        Log::info($e);
-        throw new HttpResponseException(response()->json(["message" => $message], 500));
-    }
 
     public static function responseObject($data, $message = '', $code = 200)
     {
